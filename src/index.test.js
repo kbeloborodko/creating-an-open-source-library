a6048934f72d490824e270cbefa65a74e363f2cf
kbeloborodko <kbeloborodko@gmail.com>
2017-01-16 17:40:29 +0200

    First test has been added along with some typo fixes

--- src/index.test.js
+++ src/index.test.js
@@ -1,9 +1,20 @@
 'use strict';
 
 var expect = require('chai').expect;
+var openSourceLibrary = require('./index.js');
 
 describe('starwars-names', function () {
-  it('should work', function () {
+  it('should have a list of all available names', function () {
+    expect(isArrayOfStrings(openSourceLibrary.all)).to.be.true;
+  });
+
+  it('should allow me to return a random name from the list', function () {
     expect(true).to.be.true;
   });
 });
+
+function isArrayOfStrings (array) {
+  return array.every(function (item) {
+    return typeof item === 'string';
+  });
+}
