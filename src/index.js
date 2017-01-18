ec228947cf6f697b55f55db4cd2da97b077f8f25
kbeloborodko <kbeloborodko@gmail.com>
2017-01-18 10:48:26 +0200

    Changing star wars names to breaking bad names

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 'use strict';
 
 var uniqueRandomArray = require('unique-random-array');
-var starWarsNames = require('./starwars-names.json');
+var breakingBadNames = require('./breaking-bad-names.json');
 
 module.exports = {
-  all: starWarsNames,
-  random: uniqueRandomArray(starWarsNames)
+  all: breakingBadNames,
+  random: uniqueRandomArray(breakingBadNames)
 };
--- src/index.test.js
+++ src/index.test.js
@@ -1,16 +1,16 @@
 'use strict';
 
 var expect = require('chai').expect;
-var openSourceLibrary = require('./index.js');
+var breakingBadNames = require('./index.js');
 
-describe('starwars-names', function () {
+describe('breaking-bad-names', function () {
   it('should have a list of all available names', function () {
-    expect(isArrayOfStrings(openSourceLibrary.all)).to.be.true;
+    expect(isArrayOfStrings(breakingBadNames.all)).to.be.true;
   });
 
   it('should allow me to return a random name from the list', function () {
     expect(
-      openSourceLibrary.all.indexOf(openSourceLibrary.random()) > -1
+      breakingBadNames.all.indexOf(breakingBadNames.random()) > -1
     )
       .to.be.true;
   });
